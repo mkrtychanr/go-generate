9025cda39acba1be1572500cb825a3b606075798
Raffi Mkrtychan <mkrtychanr@Raffis-MacBook-Air.local>
2023-09-26 19:20:30 +0300

    feat: crazy shit

--- pkg/file1.go
+++ pkg/file1.go
@@ -3,12 +3,13 @@ package pkg
 import (
 	"fmt"
 	"os/exec"
+	"path/filepath"
 	"runtime"
 )
 
 func GetFiles() {
 	_, filename, _, _ := runtime.Caller(0)
-	cmd := exec.Command("ls", filename)
+	cmd := exec.Command("ls", filepath.Dir(filename))
 	stdout, err := cmd.Output()
 	if err != nil {
 		fmt.Println("failed")
